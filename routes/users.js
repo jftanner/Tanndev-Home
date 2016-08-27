f58cf9a127ad42f3f57f7847c5f43bdcbf55cf2c
James Tanner <james.tanner@tanndev.com>
2016-08-27 17:53:39 -0400

    Initial, default Express config.

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016 James Tanner
+ */
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
