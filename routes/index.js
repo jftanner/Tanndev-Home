0f06e2e5ab23238e41bc99b216eb88e0870ff810
james.tanner@tanndev.com <james.tanner@tanndev.com>
2016-08-27 21:47:37 -0400

    Experimented with adding a new field in index.jade.

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tanndev' });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Tanner Developments', message: "Coming soon!"});
 });
 
 module.exports = router;
